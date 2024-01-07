dac13f26e0b2dc15e610c281915207ada62c9578
Jeremy Heng <jeremyhengjm@gmail.com>
2024-01-07 14:07:02 +0800

    def normal_logpdf

--- DiffusionBridge/utils.py
+++ DiffusionBridge/utils.py
@@ -22,8 +22,13 @@ def normal_logpdf(x, mu, sigmasq):
     """
     
     d = x.shape[1]
-    constants = - 0.5 * d * torch.log(torch.tensor(2 * math.pi)) - 0.5 * d * torch.log(sigmasq)
-    logdensity = constants - 0.5 * torch.sum((x - mu)**2, 1) / sigmasq
+    if len(sigmasq.shape) == 0:
+        constants = - 0.5 * d * torch.log(torch.tensor(2 * math.pi)) - 0.5 * d * torch.log(sigmasq)
+        logdensity = constants - 0.5 * torch.sum((x - mu)**2, 1) / sigmasq
+        
+    if len(sigmasq.shape) == 1:
+        constants = - 0.5 * d * torch.log(torch.tensor(2 * math.pi)) - 0.5 * torch.sum(torch.log(sigmasq))
+        logdensity = constants - 0.5 * torch.sum((x - mu)**2 / sigmasq, 1) 
     
     return logdensity
 
